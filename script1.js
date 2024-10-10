234a38ffabe110e6b059dcfd0165a33e6179549d
Hignem <81192237+Hignem@users.noreply.github.com>
2024-10-10 22:50:11 +0000

    finish lab1

--- script1.js
+++ script1.js
@@ -2,18 +2,33 @@ function myFunction() {
   document.getElementById("ex1_content").innerHTML = "0,1,2,3,4,5,6,7,8,9";
 }
 
-function validateNumber(number){
-  if (number.length !== 9){
-    return "Dlugosc numeru musi byc rowna 9";
+function validateNumber(number) {
+  const tempResponse = document.getElementById("ex2_content");
+  if (number.length !== 9) {
+    tempResponse.textContent = "Dlugosc numeru musi byc rowna 9";
+    return;
   }
 
-  if(/[a-zA-z]/.test(number)){
-    return "Numer nie moze zawierac liter";
+  if (/[a-zA-z]/.test(number)) {
+    tempResponse.textContent = "Numer nie moze zawierac liter";
+    return;
   }
 
-  if(/[^0-9]/.test(number)){
-    return "Numer nie moze zawierac znakow specjalnych";
-  }
+  if (/[^0-9]/.test(number)) {
+    tempResponse.textContent = "Numer nie moze zawierac znakow specjalnych";
+    return;
+  } else tempResponse.textContent = "Numer telefonu jest poprawny";
+}
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
-  return "Numer telefonu jest poprawny";
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
 }
